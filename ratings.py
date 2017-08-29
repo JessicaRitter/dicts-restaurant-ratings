7a1f77114b9e042bf36bf02c4cdde5b0cb34cd79
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-28 17:35:44 -0700

    added numeric checking

--- ratings.py
+++ ratings.py
@@ -15,9 +15,16 @@ def restaurant_rating_data(filename):
 # restaurant_rating_data("scores.txt")
 def user_rating(restaurant_ratings_dict):
     user_restaurant = raw_input("What restaurant do you want to rate?\n")
-    user_restaurant_score = raw_input("Give it a score up to 5\n")
+    # user_restaurant_score = raw_input("Give it a score (between 1 and 5)\n")
+    user_restaurant_score = None
 
-    restaurant_ratings_dict[user_restaurant] = user_restaurant_score
+    while user_restaurant_score is None or user_restaurant_score not in range(5):
+        user_restaurant_score = int(raw_input("Give it a score (between 1 and 5)\n"))
+        if user_restaurant_score in range(5):
+            restaurant_ratings_dict[user_restaurant] = user_restaurant_score
+        else:
+            print "Try again, that's not a number between 1 and 5"
+
     ratings_in_order = sorted(restaurant_ratings_dict)
 
     for k in ratings_in_order:
